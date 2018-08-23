d411ef582f5bc922bbd5cd19de95dcf75ddca10f
LZJmb <42594801+LZJmb@users.noreply.github.com>
2018-08-23 10:51:12 +0800

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+/*        simple event for test         */
+/*           Written by LZJmb           */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=0, i<100; i++)
+        cout<<"Hello! Github, I'm coming!"<<endl;
+    return 0;
+}
